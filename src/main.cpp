ed63077885af563a279ff1ad914a9c4b26ec0c4e
mhuertas <manuel.huertas.lopez@gmail.com>
2019-06-08 09:22:41 +0100

    First stable version

--- src/main.cpp
+++ src/main.cpp
@@ -4,29 +4,45 @@
 #include <ESP8266WiFi.h>
 #include <ThingerESP8266.h>
 #include <ThingerSmartConfig.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+#include <LiquidCrystal_I2C.h>
 
-#define usuario "***"
-#define device_Id "***"
-#define device_credentials "***"
+#define usuario "mhuertas"
+#define device_Id "ProMeteo"
+#define device_credentials "7no8ne10cs&K"
+#define SEALEVELPRESSURE_HPA (1013.25)
 
-const char WiFi_ssid[]="***";
-const char WiFi_password[]="***";
+const char WiFi_ssid[]="MOVISTAR_D659";
+const char WiFi_password[]="gQK9NJ6amoPbTMfcqz67";
 
 ThingerESP8266 thing(usuario, device_Id, device_credentials);
 
+Adafruit_BME280 bme;
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
 void setup() {
+
   Serial.begin(115200);
   Serial.setTimeout(2000);
 
   while(!Serial) {}
 
+  Wire.pins(D3, D2);
+  Wire.begin(D3, D2); 
+  bme.begin(0x76);
+  lcd.begin(D3,D2);
+  lcd.noBacklight();
+  lcd.noDisplay();
+ 
   thing.add_wifi(WiFi_ssid, WiFi_password);
 
   thing["Meteo"] >> [](pson& out){
-    out["Temperatura"] =0;
-    out["Humedad"] =0;
-    out["Pressure"] =0;
-    out["Altitude"] =0;
+    out["Temperatura"] = bme.readTemperature();
+    out["Humedad"] = bme.readHumidity();
+    out["Pressure"] = bme.readPressure() / 100.0F;
+    out["Altitude"] = bme.readAltitude(SEALEVELPRESSURE_HPA);
   };
 
   thing.handle();
@@ -36,5 +52,21 @@ void setup() {
   ESP.deepSleep(1000000 * 30, WAKE_RF_DEFAULT); // 60 seconds
 }
 
+void debug() {
+
+  Serial.print(" temperature\t");
+  Serial.print(bme.readTemperature());
+  Serial.print(" humidity\t");
+  Serial.print(bme.readHumidity());
+  Serial.print(" pressure\t");
+  Serial.print(bme.readPressure() / 100.0F);
+  Serial.print(" altitude\t");
+  Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.print("\n");
+
+}
+
 void loop() { 
+  //debug();
+  //delay(1000);
 }
